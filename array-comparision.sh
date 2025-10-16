5f1d28e2312be891cbfc356ca756a74c93698fc3
Divya Prabhu <85998740+422divya@users.noreply.github.com>
2025-10-16 18:13:42 +0530

    Update array-comparision.sh

--- array-comparision.sh
+++ array-comparision.sh
@@ -1,6 +1,6 @@
-In this exercise, you will need to compare three list of arrays and write the common elements of all the three arrays:
+#In this exercise, you will need to compare three list of arrays and write the common elements of all the three arrays:
 
-a=(3 5 8 10 6),b=(6 5 4 12),c=(14 7 5 7) result is the common element 5.
+#a=(3 5 8 10 6),b=(6 5 4 12),c=(14 7 5 7) result is the common element 5.
 
 
 
